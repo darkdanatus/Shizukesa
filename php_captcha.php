f7fb20425bdc4471098c8db475e468a1f64514ff
malamut2014 <malamut2014@gmail.com>
2015-08-07 16:51:20 +0300

    Update php_captcha.php

--- php_captcha.php
+++ php_captcha.php
@@ -1,14 +1,14 @@
 <?php
 session_start();
 
-$RandomStr = md5(microtime());// md5 to generate the random string
+$RandomStr = md5(microtime()); // md5 to generate the random string
 
-$ResultStr = substr($RandomStr,0,5);//trim 5 digit 
+$ResultStr = substr($RandomStr,0,5); //trim 5 digit 
 
-$NewImage =imagecreatefromjpeg("img.jpg");//image create by existing image and as back ground 
+$NewImage =imagecreatefromjpeg("img.jpg"); //image create by existing image and as back ground 
 
-$LineColor = imagecolorallocate($NewImage,233,239,239);//line color 
-$TextColor = imagecolorallocate($NewImage, 255, 255, 255);//text color-white
+$LineColor = imagecolorallocate($NewImage,0,0,0); //line color 
+$TextColor = imagecolorallocate($NewImage,204,0,0); //text color-white
 
 //imageline($NewImage,1,1,40,40,$LineColor);
 imageline($NewImage,1,100,50,0,$LineColor);
@@ -21,6 +21,6 @@ $_SESSION['capkey'] = $ResultStr;// carry the data through session
 
 header("Content-type: image/jpeg");// out out the image 
 
-imagejpeg($NewImage);//Output image to browser 
+imagejpeg($NewImage); //Output image to browser 
 
 ?>
