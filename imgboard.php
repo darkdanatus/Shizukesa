8f330008716210786f7ae0baa3ab63551d2d353e
malamut2014 <malamut2014@gmail.com>
2015-08-08 13:16:56 +0300

    I hope it works

--- imgboard.php
+++ imgboard.php
@@ -410,11 +410,11 @@ if (SHOWTITLETXT == 1) {
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="jp"><head>
 <meta name="description" content="'.S_DESCR.'"/>
 <meta http-equiv="content-type"  content="text/html;charset=utf-8" />
-<script type="text/javascript" src="futaba.js"></script>
 <!-- meta HTTP-EQUIV="pragma" CONTENT="no-cache" -->
 <link REL="SHORTCUT ICON" HREF="/favicon.ico">
-<link rel="stylesheet" type="text/css" href="'.CSSFILE.'" title="Yotsuba" />
 <link rel="alternate stylesheet" type="text/css" href="'.CSS2FILE.'" title="Futaba" disabled />
+<link rel="stylesheet" type="text/css" href="'.CSSFILE.'" title="Yotsuba" />
+<script type="text/javascript" src="futaba.js"></script>
 <title>'.TITLE.'</title>
 <script language="JavaScript"><!--
 function l(e){var P=getCookie("pwdc"),N=getCookie("namec"),i;with(document){for(i=0;i<forms.length;i++){if(forms[i].pwd)with(forms[i]){pwd.value=P;}if(forms[i].name)with(forms[i]){name.value=N;}}}};onload=l;function getCookie(key, tmp1, tmp2, xx1, xx2, xx3) {tmp1 = " " + document.cookie + ";";xx1 = xx2 = 0;len = tmp1.length;	while (xx1 < len) {xx2 = tmp1.indexOf(";", xx1);tmp2 = tmp1.substring(xx1 + 1, xx2);xx3 = tmp2.indexOf("=");if (tmp2.substring(0, xx3) == key) {return(unescape(tmp2.substring(xx3 + 1, xx2 - xx1 - 1)));}xx1 = xx2 + 1;}return("");}
