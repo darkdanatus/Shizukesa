273e0942b73ec1ca3971c17578904193f5148572
malamut2014 <malamut2014@gmail.com>
2015-08-08 05:33:11 +0300

    Update futaba.js

--- futaba.js
+++ futaba.js
@@ -1,1 +1,19 @@
+// Взято из вакабы
+function set_stylesheet(styletitle,norefresh)
+{
+	set_cookie("wakabastyle",styletitle,365);
 
+	var links=document.getElementsByTagName("link");
+	var found=false;
+	for(var i=0;i<links.length;i++)
+	{
+		var rel=links[i].getAttribute("rel");
+		var title=links[i].getAttribute("title");
+		if(rel.indexOf("style")!=-1&&title)
+		{
+			links[i].disabled=true; // IE needs this to work. IE needs to die.
+			if(styletitle==title) { links[i].disabled=false; found=true; }
+		}
+	}
+	if(!found) set_preferred_stylesheet();
+}
