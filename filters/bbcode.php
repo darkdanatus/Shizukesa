5bda6e685ec4bcf7d4003ac687ca3a79fc155c2f
malamut2014 <malamut2014@gmail.com>
2015-09-15 22:10:05 +0300

    Update bbcode.php

--- filters/bbcode.php
+++ filters/bbcode.php
@@ -4,7 +4,7 @@
     $com = preg_replace("/\*\*(.*)\*\*/Usi", "<b>\\1</b>", $com);
     $com = preg_replace("/\*(.*)\*/Usi", "<i>\\1</i>", $com);
     $com = preg_replace("/\%\%(.*)\%\%/Usi", "<span title=\"spoiler\" style=\"color: #000000; background-color: #000000;\" class=\"spoiler\" onmouseover=\"this.style.color='#FFFFFF';\" onmouseout=\"this.style.color=this.style.backgroundColor='#000000'\">\\1</span>", $com); 
-    $com = preg_replace("/\&\&(.*)\&\&/Usi", "<iframe width=\"425\" height=\"344\" src=\"https:/\/www.youtube.com/embed/\\1\" frameborder=\"0\" allowfullscreen></iframe>", $com);
+    $com = preg_replace("/\&\&(.*)\&\&/Usi", "<iframe width=\"425\" height=\"344\" src=\"https://www.youtube.com/embed/\\1\" frameborder=\"0\" allowfullscreen></iframe>", $com);
     // BBcode
     $com = preg_replace("/\[b\](.*)\[\/b\]/Usi", "<b>\\1</b>", $com);
     $com = preg_replace("/\[u\](.*)\[\/u\]/Usi", "<u>\\1</u>", $com);
@@ -15,5 +15,5 @@
     $com = preg_replace("/\[size=(.*)\](.*)\[\/size\]/Usi", "<span style=\"font-size:\\1ex\">\\2</span>", $com);
     $com = preg_replace("/\[quote](.*)\[\/quote\]/Usi", "<div>Quote:</div><div style=\"border:solid 1px;\">\\1</div>", $com);
     /* Auto Linker */
-    $com = ereg_replace("(https?|ftp|news|http)(://[[:alnum:]\+\$\;\?\.%,!#~*/:@&=_-]+)","<a href=\"\\1\\2\">\\1\\2</a>",$com);	
+    $com = ereg_replace("(http?|ftp|news|http)(://[[:alnum:]\+\$\;\?\.%,!#~*/:@&=_-]+)","<a href=\"\\1\\2\">\\1\\2</a>",$com);	
 ?>
