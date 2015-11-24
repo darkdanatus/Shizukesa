7a263d573c83825d39369c4fe89b1daea94d3c94
malamut2014 <malamut2014@gmail.com>
2015-11-24 14:41:53 +0300

    Fix custom css feature

--- jquery/extra/custom_css.js
+++ jquery/extra/custom_css.js
@@ -6,8 +6,12 @@ jq.custom_css = {
 		this.config = {
 			enabled: jq.suite_settings && jq_jsuite_getCookie("custom_css_enabled") ? jq_jsuite_getCookie("custom_css_enabled") === "true" : false
 		}
-		jq.suite_settings && jq.suite_settings.info.push({menu:{category:'Прочее',read:this.config.enabled,variable:'custom_css_enabled',label:'Пользовательский CSS',hover:'Include your own CSS rules'},popup:{label:'[Изменить]',title:'Пользовательский CSS',type:'textarea',variable:'custom_css_defined',placeholder:'Вставьте кастомный CSS сюда.'}});
+		jq.suite_settings && jq.suite_settings.info.push({menu:{category:'Прочее',read:this.config.enabled,variable:'custom_css_enabled',label:'Свой CSS',hover:'Применить свой CSS для данной доски'},popup:{label:'[Редактировать]',title:'Пользовательский CSS',type:'textarea',variable:'custom_css_defined',placeholder:'body {}'}});
+		this.update();
+	},
+	update: function() {
 		if (jq.custom_css.config.enabled) {
 			$("<style type='text/css'>"+jq_jsuite_getCookie("custom_css_defined")+"</style>").appendTo("head");
 		}
 	}
+}
